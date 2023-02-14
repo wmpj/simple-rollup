6adf6412f636813c594cc83b8321f7bc99ce5496
김성집 <jiby@wemakeprice.com>
2023-02-14 15:48:40 +0900

    declare ts file (미해결) 추가

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,39 @@
 const commonjs = require('@rollup/plugin-commonjs');
 const resolve = require('@rollup/plugin-node-resolve');
 const { terser } = require('rollup-plugin-terser');
+const typescript = require('rollup-plugin-typescript2');
+const dts = require('rollup-plugin-dts');
 
-module.exports = {
-  input: "src/main.js",
-  output: [
-    {
-      file: "dist/bundle.cjs",
-      format: "cjs",
-    },
-    {
-      file: "dist/bundle.mjs",
-      format: "esm",
-    },
-    {
-      file: 'dist/bundle.min.js',
-      format: 'iife',
-      name: 'version',
-      plugins: [ terser() ],
-    },
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-  ]
-};
+module.exports = [
+  {
+    input: 'src/main.ts',
+    output: [
+      {
+        file: 'dist/bundle.cjs',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/bundle.mjs',
+        format: 'esm',
+      },
+      {
+        file: 'dist/bundle.min.js',
+        format: 'iife',
+        name: 'version',
+        plugins: [ terser() ],
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript(),
+    ],
+  },
+  {
+    input: './src/index.d.ts',
+    output: [{file: 'dist/index.d.ts', format: 'es'}],
+    plugin: [
+      dts.default(),
+    ],
+  },
+];
